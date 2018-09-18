164494bf94016dada7203303b684aecf37d83622
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-18 17:31:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-}
+  document.body.addEventListener('keydown', function(e){
+     console.log(`button pressed`, e)
+    // debugger
+    const key = e.key;
+
+    for (let i = 0; i < codes.length; i++){
+      if (key === codes[i]) {
+        console.log(`should be letter`, codes[i])
+        debugger
+          if (i === codes.length) {
+          alert("Hurray!");
+          i = 0;
+          }
+      } else {
+        i = 0;
+        }
+    }
+  })
+};
